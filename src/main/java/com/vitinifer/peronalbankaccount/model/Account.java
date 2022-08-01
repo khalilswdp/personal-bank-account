9c75cfa85690cc2d3f39b4f3c94f7112c1bf8f1c
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-08-01 17:15:52 +0100

    Add starting classes

--- src/main/java/com/vitinifer/peronalbankaccount/model/Account.java
+++ src/main/java/com/vitinifer/peronalbankaccount/model/Account.java
@@ -1,2 +1,83 @@
-package com.vitinifer.peronalbankaccount.model;public class Account {
+package com.vitinifer.peronalbankaccount.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+    private String name;
+    private String iban;
+    private double balance;
+    private List<Operation> operations;
+
+    // Constructor
+    public Account(String name, String iban, double balance) {
+        this.name = name;
+        this.iban = iban;
+        this.balance = balance;
+        this.operations = new ArrayList<Operation>();
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Operation> getOperations() {
+        return operations;
+    }
+
+    // Setters
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void addOperation(Operation operation) {
+        this.operations.add(operation);
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "Account [name=" + name + ", iban=" + iban + ", balance=" + balance + ", operations=" + operations + "]";
+    }
+
+    public static class AccountBuilder {
+        private String name;
+        private String iban;
+        private double balance;
+
+        public AccountBuilder(String name, String iban, double balance) {
+            this.name = name;
+            this.iban = iban;
+            this.balance = balance;
+        }
+
+        public AccountBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public AccountBuilder iban(String iban) {
+            this.iban = iban;
+            return this;
+        }
+
+        public AccountBuilder balance(double balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public Account build() {
+            return new Account(name, iban, balance);
+        }
+    }
 }
--- src/main/java/com/vitinifer/peronalbankaccount/model/Operation.java
+++ src/main/java/com/vitinifer/peronalbankaccount/model/Operation.java
@@ -1,2 +1,83 @@
-package com.vitinifer.peronalbankaccount.model;public class Operation {
+package com.vitinifer.peronalbankaccount.model;
+
+public class Operation {
+    private String type;
+    private double amount;
+    private double balance;
+
+    // Constructor
+    public Operation(String type, double amount, double balance) {
+        this.type = type;
+        this.amount = amount;
+        this.balance = balance;
+    }
+
+    // Getters
+    public String getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Setters
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Operation{" +
+                "type='" + type + '\'' +
+                ", amount=" + amount +
+                ", balance=" + balance +
+                '}';
+    }
+
+    // Builder
+    public static class OperationBuilder {
+        private String type;
+        private double amount;
+        private double balance;
+
+        public OperationBuilder(String type, double amount, double balance) {
+            this.type = type;
+            this.amount = amount;
+            this.balance = balance;
+        }
+
+        public OperationBuilder type(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public OperationBuilder amount(double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public OperationBuilder balance(double balance) {
+            this.balance = balance;
+            return this;
+        }
+
+        public Operation build() {
+            return new Operation(type, amount, balance);
+        }
+    }
+
 }
