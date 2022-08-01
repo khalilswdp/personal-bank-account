35dd3763b959c9df23b4b23ff6ccbe1ec3ada5cb
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-08-01 18:12:19 +0100

    Getters for AccountRepository

--- src/main/java/com/vitinifer/peronalbankaccount/repository/AccountRepository.java
+++ src/main/java/com/vitinifer/peronalbankaccount/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.vitinifer.peronalbankaccount.repository;
 
 import com.vitinifer.peronalbankaccount.model.Account;
+import com.vitinifer.peronalbankaccount.model.Operation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,5 +33,21 @@ public class AccountRepository {
     // Setters
     public void addAccount(Account account) {
         this.accounts.add(account);
+    }
+
+    public Account getAccount(String iban) {
+        return accounts.stream()
+                .filter(account -> account.getIban().equals(iban))
+                .findFirst()
+                .get();
+    }
+    public double getBalance(String iban) {
+        return getAccount(iban)
+                .getBalance();
+    }
+
+    public List<Operation> getOperations(String iban) {
+        return getAccount(iban)
+                .getOperations();
     }
 }
