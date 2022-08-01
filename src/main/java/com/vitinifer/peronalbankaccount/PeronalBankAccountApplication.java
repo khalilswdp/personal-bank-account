209926da9a903564a8e00ec5342cd7041e79cdae
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-08-01 17:15:16 +0100

    Remove Spring Boot

--- src/main/java/com/vitinifer/peronalbankaccount/PeronalBankAccountApplication.java
+++ src/main/java/com/vitinifer/peronalbankaccount/PeronalBankAccountApplication.java
@@ -1,13 +1,9 @@
 package com.vitinifer.peronalbankaccount;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class PeronalBankAccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeronalBankAccountApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 
 }
--- src/test/java/com/vitinifer/peronalbankaccount/PeronalBankAccountApplicationTests.java
+++ src/test/java/com/vitinifer/peronalbankaccount/PeronalBankAccountApplicationTests.java
@@ -1,13 +1,5 @@
 package com.vitinifer.peronalbankaccount;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class PeronalBankAccountApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
